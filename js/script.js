cc31182f86a8d7ff2c193bd15bbfdde6fe7923f9
RenmasAS <maximo.huaman2003@gmail.com>
2024-09-19 11:47:02 -0500

    Avance en el cambio de los colores de día y noche

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Formulario Enviado');
+const toggleButton = document.getElementById('toggle-mode');
+const body = document.body;
+
+toggleButton.addEventListener('click', function() {
+    body.classList.toggle('night-mode');
+    const icon = toggleButton.querySelector('i');
+
+    if (body.classList.contains('night-mode')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
 });
+
